1066fa48492378d9800736d009f4413f58c2a1fa
Taein5415 <kskk5050@naver.com>
2025-04-30 00:11:12 +0900

    [feat #171] 채팅 api 수정

---
+++ devpath-community-server/src/main/java/com/freepath/devpath/chatting/query/dto/response/WaitingChattingRoomDTO.java
@@ -0,0 +1,10 @@
+package com.freepath.devpath.chatting.query.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class WaitingChattingRoomDTO {
+    private String chattingRoomTitle;
+}
